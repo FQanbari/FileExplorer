c9d2372ec248c0f71b076cbec01604d384f2a5eb
Fatemeh <fatemeh.ganbari1993@gmail.com>
2023-12-16 18:36:46 +0330

    Add project files.

---
+++ FileExplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("=== BOOTCAMP SEARCH :: An extendible command-line search tool ===");
+Console.WriteLine();
+
+Console.WriteLine("1. Search for files");
+Console.WriteLine("2. Manage Extensions");
+Console.WriteLine("3. View search history");
+Console.WriteLine();
+Console.WriteLine("4. Exit");
+Console.WriteLine();
+Console.WriteLine("NOTE: There was a problem with loading 2 extensions. View them in Manage Extensions section.");
+Console.WriteLine();
+Console.WriteLine("Please input an option:");
