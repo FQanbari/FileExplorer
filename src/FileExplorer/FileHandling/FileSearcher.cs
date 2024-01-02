08d7425ce1ad2eaa70236668b6240fe112052930
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-01-02 15:16:23 +0330

    change name of file

--- src/FileExplorer/FileHandling/FileSearcher.cs
+++ src/FileExplorer/FileHandling/FileSearcher.cs
@@ -14,7 +14,7 @@ public class FileSearcher : IFileSearcher
 
     public FileSearcher()
     {
-        _historyFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "_searchHistory.json");
+        _historyFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "searchHistory.json");
         _searchHistory = LoadSearchHistory();
     }
     public List<string> SearchFiles(string rootDirectory, List<IExtension> plugins, string searchQuery)
